f40d0894fdf6acf8fcab24ff7dd3707a5c0d7de8
pazet <ebengtea25@gmail.com>
2024-01-27 07:10:06 +0700

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Fajar Maulana</span> a passionate content creator and graphic designer with big dreams and a digital pen in hand. With a background in digital art and experience in producing engaging content, I have created unique visual worlds online."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
